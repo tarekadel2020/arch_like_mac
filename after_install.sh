53702cd2461360914a2828ff535ad76a71a15751
dev <78466267+tarekadel2020@users.noreply.github.com>
2021-05-08 15:54:08 +0200

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,9 +1,31 @@
 #!/bin/bash -x
 set -e
 
-########### install pamace-aur ######################
+##################### install pamace-aur #################################
 cd $HOME
 git clone https://aur.archlinux.org/pamac-aur.git
 cd ./pamac-aur
 makepkg -si 
-#####################################################
+###########################################################################
+
+############ install vala-panel-appmenu-common-git  #######################
+cd $HOME
+git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+cd ./vala-panel-appmenu-common-git
+makepkg -si 
+###########################################################################
+
+########### install vala-panel-appmenu-registrar-git ######################
+cd $HOME
+git clone https://aur.archlinux.org/vala-panel-appmenu-registrar-git.git 
+cd ./vala-panel-appmenu-registrar-git
+makepkg -si 
+###########################################################################
+
+######################## install vala-panel-appmenu-xfce-git ##############
+cd $HOME
+git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+cd ./vala-panel-appmenu-xfce-git
+makepkg -si 
+
+############################################################################
