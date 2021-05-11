db700e74a5d009517e8fb49a45769a228542754c
dev <78466267+tarekadel2020@users.noreply.github.com>
2021-05-11 15:20:36 +0200

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,31 +1,40 @@
 #!/bin/bash -x
 set -e
 
-##################### install pamace-aur #################################
-cd $HOME
-git clone https://aur.archlinux.org/pamac-aur.git
-cd ./pamac-aur
-makepkg -si 
-###########################################################################
+pamace-aur(){
+    cd $HOME
+    git clone https://aur.archlinux.org/pamac-aur.git
+    cd ./pamac-aur
+    makepkg -si 
+}
 
-############ install vala-panel-appmenu-common-git  #######################
-cd $HOME
-git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
-cd ./vala-panel-appmenu-common-git
-makepkg -si 
-###########################################################################
+vala-panel-appmenu-common-git(){
+      cd $HOME
+      git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+      cd ./vala-panel-appmenu-common-git
+      makepkg -si 
+}
 
-########### install vala-panel-appmenu-registrar-git ######################
-cd $HOME
-git clone https://aur.archlinux.org/vala-panel-appmenu-registrar-git.git 
-cd ./vala-panel-appmenu-registrar-git
-makepkg -si 
-###########################################################################
+vala-panel-appmenu-registrar-git(){
+      cd $HOME
+      git clone https://aur.archlinux.org/vala-panel-appmenu-registrar-git.git 
+      cd ./vala-panel-appmenu-registrar-git
+      makepkg -si 
+}
 
-######################## install vala-panel-appmenu-xfce-git ##############
-cd $HOME
-git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
-cd ./vala-panel-appmenu-xfce-git
-makepkg -si 
+vala-panel-appmenu-xfce-git(){
+      cd $HOME
+      git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+      cd ./vala-panel-appmenu-xfce-git
+      makepkg -si 
 
-############################################################################
+}
+
+Main(){
+      pamace-aur
+      vala-panel-appmenu-common-git
+      vala-panel-appmenu-registrar-git
+      vala-panel-appmenu-xfce-git
+}
+
+Main
