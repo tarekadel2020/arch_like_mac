58a0fa231d9ba4ac4e59c2761526290ee3f94c64
dev <78466267+tarekadel2020@users.noreply.github.com>
2021-05-12 15:29:05 +0200

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -9,28 +9,35 @@ pamace-aur(){
 }
 
 vala-panel-appmenu-common-git(){
-      cd $HOME
-      git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
-      cd ./vala-panel-appmenu-common-git
-      makepkg -si 
+      #cd $HOME
+      #git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+      #cd ./vala-panel-appmenu-common-git
+      #makepkg -si
+      pamac build vala-panel-appmenu-common-git
 }
 
 vala-panel-appmenu-registrar-git(){
-      cd $HOME
-      git clone https://aur.archlinux.org/vala-panel-appmenu-registrar-git.git 
-      cd ./vala-panel-appmenu-registrar-git
-      makepkg -si 
+      #cd $HOME
+      #git clone https://aur.archlinux.org/vala-panel-appmenu-registrar-git.git 
+      #cd ./vala-panel-appmenu-registrar-git
+      #makepkg -si 
+      pamac build vala-panel-appmenu-registrar-git
 }
 
 vala-panel-appmenu-xfce-git(){
-      cd $HOME
-      git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
-      cd ./vala-panel-appmenu-xfce-git
-      makepkg -si 
+      #cd $HOME
+      #git clone https://aur.archlinux.org/vala-panel-appmenu-xfce-git.git
+      #cd ./vala-panel-appmenu-xfce-git
+      #makepkg -si 
+      pamac build vala-panel-appmenu-xfce-git
+}
 
+mugshot(){
+      pamac build mugshot
 }
 
 Main(){
+      mugshot
       pamace-aur
       vala-panel-appmenu-common-git
       vala-panel-appmenu-registrar-git
